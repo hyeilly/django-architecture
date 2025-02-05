d8eb9b9d8fe06a3a51b4c6893f7686fadf5c7e2d
hyeilly <dev.jiine@gmail.com>
2025-02-05 15:20:01 +0900

    DB 이름도 받을 수 있도록 수정

--- core/common/db/mongo_pymongo.py
+++ core/common/db/mongo_pymongo.py
@@ -1,33 +1,45 @@
 from pymongo import MongoClient
 import os
+from django.conf import settings
 
 class PyMongoConnection:
-    def __init__(self):
+    _instance = None
+    _client = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(PyMongoConnection, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self, db_name=None):
         """pymongo 연결 정보 초기화"""
-        self.client = None
-        self.db = None
+        if self._client is None:
+            self._client = MongoClient(settings.MONGODB_SETTINGS['url'])
+        self._db = self._client[db_name or settings.MONGODB_SETTINGS['db_name']]
         self.mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017")
         self.db_name = os.getenv("MONGO_DB_NAME", "mydatabase")
 
     def __enter__(self):
         """with 문에서 pymongo 연결"""
-        self.client = MongoClient(self.mongo_uri)
-        self.db = self.client[self.db_name]
         print("✅ [pymongo] MongoDB 연결 성공")
         return self  # pymongo 인스턴스를 반환하여 사용 가능
 
     def __exit__(self, exc_type, exc_value, traceback):
         """with 문에서 pymongo 연결 해제"""
-        if self.client:
-            self.client.close()
-            self.client = None
-            self.db = None
+        if self._client:
+            self._client.close()
+            self._client = None
             print("❌ [pymongo] MongoDB 연결 종료")
 
     def get_database(self):
         """데이터베이스 객체 반환"""
-        return self.db
+        return self._db
 
     def get_collection(self, collection_name):
         """컬렉션 객체 반환"""
-        return self.db[collection_name] if self.db else None
+        return self._db[collection_name]
+
+    def close(self):
+        if self._client:
+            self._client.close()
+            self._client = None
