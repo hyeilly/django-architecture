088b52d3f629085b42b0eb07b283ad63e18c4b19
hyeilly <dev.jiine@gmail.com>
2025-02-05 15:44:58 +0900

    edit

--- core/common/db/mongo_pymongo.py
+++ core/common/db/mongo_pymongo.py
@@ -6,7 +6,7 @@ class PyMongoConnection:
     _instance = None
     _client = None
 
-    def __new__(cls):
+    def __new__(cls, *args, **kwargs):
         if cls._instance is None:
             cls._instance = super(PyMongoConnection, cls).__new__(cls)
         return cls._instance
